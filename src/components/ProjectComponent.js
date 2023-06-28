6f6eae96a4a37f6ecf8275badaec1dc816cdf73f
darylbg <darylxcuf@gmail.com>
2023-06-28 15:43:26 +0100

    added new project cards/styling

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -2,25 +2,53 @@ import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import ProjectPills from './subComponents/ProjectPills';
 import { ExternalLink, Code } from 'react-feather';
-// import '../assets/css/ProjectComponent.css';
-import '../assets/css/ProjectComponent2.css'
+import '../assets/css/ProjectComponent.css';
+// import '../assets/css/ProjectComponent2.css'
 
 function ProjectComponent({ index, title, description, img, pills, liveLink, codeLink }) {
-    const [ isHovered, setIsHovered] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-      };
-    
-      const handleMouseLeave = () => {
-        setIsHovered(false);
-      };
-    
-      const Styles = {
-        background: 'linear-gradient(90deg, rgba(10,25,47,1) 0%, rgba(38,61,102,1) 100%)',
-      }
-
     return(
+            <div className="card mb-3 project-card">
+                <div className="row g-0">
+                { index%2 === 0 ? (
+                    <>
+                    <div className="col-md-6">
+                        <div className="card-body text-start">
+                            <h4 className='card-title'>{`Project ${index + 1}`}</h4>
+                            <h3 className="card-title">{title}</h3>
+                            <p className="card-text">{description}</p>
+                            <ul>
+                                <li><Link>View Project</Link></li>
+                                <li><Link>View Code</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-6 card-image">
+                        <img src={require('../assets/images/ReadSky.PNG')} className="img-fluid rounded-start" alt="..."></img>
+                        <div className='img-border'></div>
+                    </div>
+                    </>
+                    ) : (
+                    <>
+                    <div className="col-md-6 card-image">
+                        <img src={require('../assets/images/ReadSky.PNG')} className="img-fluid rounded-start" alt="..."></img>
+                        <div className='img-border'></div>
+                    </div>
+                        <div className="col-md-6">
+                        <div className="card-body text-start">
+                            <h4 className='card-title'>{`Project ${index + 1}`}</h4>
+                            <h3 className="card-title">{title}</h3>
+                            <p className="card-text">{description}</p>
+                            <ul>
+                                <li><Link>View Project</Link></li>
+                                <li><Link>View Code</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    </>
+                    )}
+                </div>
+            </div>
+
         // <div className="project-card card mb-3" id={index} style={Styles}>
         //     { index%2 === 0 ? (
         //     <div className="row g-0">
@@ -87,24 +115,24 @@ function ProjectComponent({ index, title, description, img, pills, liveLink, cod
         //     </div>
         //     )}
         // </div>
-        <section style={{paddingBlock: '2rem'}}>
-            <div className="projects container">
-                <article className="project" id={index}>
-                <div className="project__img-container">
-                    <img className="project__img" src='https://images.unsplash.com/photo-1544474560-5b2a788d024a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI3NjU2OTU&ixlib=rb-4.0.3&q=80&w=800' alt=''></img>
-                </div>
-                <div className="project__content grid-flow">
-                    <h3 className="project__title">E Commerce Website</h3>
-                    <ul className="project__tags flex-group" role="list">
-                    <li className="project__tag">NextJS</li>
-                    <li className="project__tag">TypeScript</li>
-                    </ul>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi reprehenderit ipsa officia dolore recusandae cumque tenetur, cupiditate est? Suscipit voluptatibus nihil, nam quisquam illo architecto laudantium perspiciatis ipsa nesciunt dolor!</p>
-                    <Link className="project__cta" to="exmaple.com" target="_blank" rel="noopener noreferrer">View project</Link>
-                </div>
-                </article>
-            </div>
-            </section>
+        // <section style={{paddingBlock: '2rem'}}>
+        //     <div className="projects container">
+        //         <article className={`project ${gridTemplateAreas}`}>
+        //         <div className="project__img-container">
+        //             <img className="project__img" src='https://images.unsplash.com/photo-1544474560-5b2a788d024a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI3NjU2OTU&ixlib=rb-4.0.3&q=80&w=800' alt=''></img>
+        //         </div>
+        //         <div className="project__content grid-flow">
+        //             <h3 className="project__title">E Commerce Website</h3>
+        //             <ul className="project__tags flex-group" role="list">
+        //             <li className="project__tag">NextJS</li>
+        //             <li className="project__tag">TypeScript</li>
+        //             </ul>
+        //             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi reprehenderit ipsa officia dolore recusandae cumque tenetur, cupiditate est? Suscipit voluptatibus nihil, nam quisquam illo architecto laudantium perspiciatis ipsa nesciunt dolor!</p>
+        //             <Link className="project__cta" to="exmaple.com" target="_blank" rel="noopener noreferrer">View project</Link>
+        //         </div>
+        //         </article>
+        //     </div>
+        //     </section>
     )
 };
 
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -4,7 +4,7 @@ import '../assets/css/Style.css';
 
 function Projects() {
     const projects = [
-        {title: 'Project 1', description: 'App using node', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {title: 'Twitter Clone', description: 'App using node', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'React', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
@@ -13,14 +13,14 @@ function Projects() {
                 {name: 'NodeJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'}
             ]
         },
-        {title: 'Project 2', description: 'App using html and css', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {title: 'Guessing Game', description: 'App using html and css', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
             ]
         },
-        {title: 'Project 3', description: 'App using javascript with mongo db integration', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {title: 'Portfolio', description: 'App using javascript with mongo db integration', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
@@ -36,9 +36,13 @@ function Projects() {
             <h3 className="section-title">04.<span> Projects ----------------</span></h3>
            </div>
            <div className="row projects-row">
-           {projects.map((project, index) => (
-                <ProjectComponent key={index} index={index} title={project.title} description={project.description} img={project.img} pills={project.pills} liveLink={project.liveLink} codeLink={project.codeLink} />
-           ))}
+            {/* <section style={{paddingBlock: '2rem'}}>
+            <div className="projects container"> */}
+            {projects.map((project, index) => (
+                    <ProjectComponent key={index} index={index} title={project.title} description={project.description} img={project.img} pills={project.pills} liveLink={project.liveLink} codeLink={project.codeLink} />
+            ))}
+            {/* </div>
+            </section> */}
            </div>
         </div>
     )
