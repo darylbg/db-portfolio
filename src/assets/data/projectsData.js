15691cedd9d31720940541482e7b8f5a4ca94948
darylbg <darylxcuf@gmail.com>
2023-06-29 12:27:45 +0100

    added styling to project cards

--- src/assets/data/projectsData.js
+++ src/assets/data/projectsData.js
@@ -1,5 +1,10 @@
 const projects = [
-        {title: 'Twitter Clone', description: 'App using node', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {
+            title: 'Twitter Clone', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra. Maecenas vitae vulputate turpis. Fusce ullamcorper risus a ex lacinia, sed blandit diam consectetur. ', 
+            img: 'ReadSky.PNG', 
+            liveLink: 'https://www.youtube.com/', 
+            codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'React', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
@@ -8,14 +13,24 @@ const projects = [
                 {name: 'NodeJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'}
             ]
         },
-        {title: 'Guessing Game', description: 'App using html and css', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {
+            title: 'Guessing Game', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra. Maecenas vitae vulputate turpis. Fusce ullamcorper risus a ex lacinia, sed blandit diam consectetur. ', 
+            img: 'ReadSky.PNG', 
+            liveLink: 'https://www.youtube.com/', 
+            codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
             ]
         },
-        {title: 'Portfolio', description: 'App using javascript with mongo db integration', img: 'ReadSky.PNG', liveLink: 'https://www.youtube.com/', codeLink: 'https://github.com/darylbg/db-portfolio',
+        {
+            title: 'Portfolio', 
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra. Maecenas vit vulputate turpis. Fusce Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra.',
+            img: 'ReadSky.PNG', 
+            liveLink: 'https://www.youtube.com/', 
+            codeLink: 'https://github.com/darylbg/db-portfolio',
             pills: [
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -1,148 +1,40 @@
 import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import ProjectPills from './subComponents/ProjectPills';
-import { ExternalLink, Code } from 'react-feather';
+import { ExternalLink, Code, ArrowRight } from 'react-feather';
 import '../assets/css/ProjectComponent.css';
 // import '../assets/css/ProjectComponent2.css'
 
 function ProjectComponent({ index, title, description, img, pills, liveLink, codeLink }) {
+    
+    const width = window.innerWidth;
+    console.log(width);
+
     return(
-            <div className="card mb-3 project-card">
-                <div className="row g-0">
-                { index%2 === 0 ? (
-                    <>
-                    <div className="col-md-6">
-                        <div className="card-body text-start">
-                            <h4 className='card-title'>{`PROJECT ${index + 1}`}</h4>
-                            <h3 className="card-title">{title}</h3>
-                            <ul>
-                                {pills.map((pill, index) => (
-                                    <ProjectPills name={pill.name} link={pill.link} key={index} />
-                                ))}  
-                            </ul>
-                            <p className="card-text">{description}</p>
-                            <ul>
-                                <li><Link>View Project</Link></li>
-                                <li><Link>View Code</Link></li>
-                            </ul>
-                        </div>
+        <div className="card mb-3 project-card">
+            <div className="row g-0">
+                <div className="col-md-6">
+                    <div className="card-body text-start">
+                        <h4 className='card-title'>{`PROJECT ${index + 1}`}</h4>
+                        <h3 className="card-title">{title}</h3>
+                        <ul className='pills-list'>
+                            {pills.map((pill, index) => (
+                                <ProjectPills name={pill.name} link={pill.link} key={index} />
+                            ))}  
+                        </ul>
+                        <p className="card-text">{description}</p>
+                        <ul className='link-list'>
+                            <li><Link>View Project<span><ArrowRight /></span></Link></li>
+                            <li><Link>View Code<span><ArrowRight /></span></Link></li>
+                        </ul>
                     </div>
-                    <div className="col-md-6 card-image">
-                        <img src={require('../assets/images/ReadSky.PNG')} className="img-fluid rounded-start" alt="..."></img>
-                        <div className='img-border'></div>
-                    </div>
-                    </>
-                    ) : (
-                    <>
-                    <div className="col-md-6 card-image">
-                        <img src={require('../assets/images/ReadSky.PNG')} className="img-fluid rounded-start" alt="..."></img>
-                        <div className='img-border'></div>
-                    </div>
-                        <div className="col-md-6">
-                        <div className="card-body text-start">
-                            <h4 className='card-title'>{`PROJECT ${index + 1}`}</h4>
-                            <h3 className="card-title">{title}</h3>
-                            <ul>
-                                {pills.map((pill, index) => (
-                                    <ProjectPills name={pill.name} link={pill.link} key={index} />
-                                ))}  
-                            </ul>
-                            <p className="card-text">{description}</p>
-                            <ul>
-                                <li><Link>View Project</Link></li>
-                                <li><Link>View Code</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                    </>
-                    )}
+                </div>
+                <div className="col-md-6 card-image">
+                    <img src={require('../assets/images/ReadSky.PNG')} className="img-fluid rounded-start" alt="..."></img>
+                    <div className='img-border'></div>
                 </div>
             </div>
-
-        // <div className="project-card card mb-3" id={index} style={Styles}>
-        //     { index%2 === 0 ? (
-        //     <div className="row g-0">
-        //         <div className="col-md-8">
-        //             <div className='card-body text-start'>
-        //                 <h5 className="card-title">{title}</h5>
-        //                 <p className="card-text">{description}</p>
-        //                 <ul>
-        //                     {pills.map((pill, index) => (
-        //                         <ProjectPills key={index} name={pill.name} link={pill.link} />
-        //                     ))}
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //         <div className="project-image col-md-4" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        //                 <img src={require(`../assets/images/${img}`)} className="img-fluid rounded-start" alt="..."></img>
-        //                 <div className={`img-hover ${isHovered ? '' : 'd-none'}`}>
-        //                     <Link to={liveLink} target='_blank'>
-        //                         <div className='hover-live-link align-middle'>
-        //                             <ExternalLink />
-        //                             <span >View live</span>
-        //                         </div>
-        //                     </Link>
-        //                     <Link to={codeLink} target='_blank'>
-        //                         <div className='hover-code-link align-middle'>
-        //                             <Code />
-        //                             <span className=''>Source code</span>
-        //                         </div>
-        //                     </Link>
-        //                 </div>
-                    
-        //         </div>
-        //     </div>
-        //     ) : (
-        //     <div className="row g-0">
-        //         <div className={"project-image col-md-4"} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        //         <img src={require(`../assets/images/${img}`)} className="img-fluid rounded-start" alt="..."></img>
-        //             <div className={`img-hover ${isHovered ? '' : 'd-none'}`}>
-        //                 <Link to={liveLink} target='_blank'>
-        //                     <div className='hover-live-link align-middle'>
-        //                         <ExternalLink />
-        //                         <span >View live</span>
-        //                     </div>
-        //                 </Link>
-        //                 <Link to={codeLink} target='_blank'>
-        //                     <div className='hover-code-link align-middle'>
-        //                         <Code />
-        //                         <span className=''>Source code</span>
-        //                     </div>
-        //                 </Link>
-        //             </div>
-        //         </div>
-        //         <div className="col-md-8">
-        //             <div className='card-body text-end'>
-        //                 <h5 className="card-title">{title}</h5>
-        //                 <p className="card-text">{description}</p>
-        //                 <ul>
-        //                     {pills.map((pill, index) => (
-        //                         <ProjectPills key={index} name={pill.name} link={pill.link} />
-        //                     ))}
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     )}
-        // </div>
-        // <section style={{paddingBlock: '2rem'}}>
-        //     <div className="projects container">
-        //         <article className={`project ${gridTemplateAreas}`}>
-        //         <div className="project__img-container">
-        //             <img className="project__img" src='https://images.unsplash.com/photo-1544474560-5b2a788d024a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODI3NjU2OTU&ixlib=rb-4.0.3&q=80&w=800' alt=''></img>
-        //         </div>
-        //         <div className="project__content grid-flow">
-        //             <h3 className="project__title">E Commerce Website</h3>
-        //             <ul className="project__tags flex-group" role="list">
-        //             <li className="project__tag">NextJS</li>
-        //             <li className="project__tag">TypeScript</li>
-        //             </ul>
-        //             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi reprehenderit ipsa officia dolore recusandae cumque tenetur, cupiditate est? Suscipit voluptatibus nihil, nam quisquam illo architecto laudantium perspiciatis ipsa nesciunt dolor!</p>
-        //             <Link className="project__cta" to="exmaple.com" target="_blank" rel="noopener noreferrer">View project</Link>
-        //         </div>
-        //         </article>
-        //     </div>
-        //     </section>
+        </div>
     )
 };
 
