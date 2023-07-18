d00efeca8f22c28d6b914f83959e7c4a49d42993
darylbg <darylxcuf@gmail.com>
2023-07-18 20:19:12 +0100

    added airbnp project

--- src/assets/data/projectsData.js
+++ src/assets/data/projectsData.js
@@ -1,16 +1,18 @@
 const projects = [
         {
-            title: 'Twitter Clone', 
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra. Maecenas vitae vulputate turpis. Fusce ullamcorper risus a ex lacinia, sed blandit diam consectetur. ', 
-            img: 'ReadSky.PNG', 
-            liveLink: 'https://www.youtube.com/', 
-            codeLink: 'https://github.com/darylbg/db-portfolio',
+            title: 'Airbnp', 
+            description: 'Thing Airbnb but for bathrooms. ', 
+            img: 'airbnp.PNG', 
+            liveLink: 'https://airbnp-website-190a74645719.herokuapp.com/', 
+            codeLink: 'https://github.com/darylbg/airbnp',
             pills: [
                 {name: 'React', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
                 {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'NodeJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'}
+                {name: 'NodeJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
+                {name: 'MongoDB', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
+                {name: 'GraphQL', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'}
             ]
         },
         {
