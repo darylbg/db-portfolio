198259146600711d001338b19ba43bca7b2a770b
darylbg <darylxcuf@gmail.com>
2023-07-28 12:39:52 +0100

    updated project details

--- src/assets/data/projectsData.js
+++ src/assets/data/projectsData.js
@@ -1,47 +1,105 @@
 const projects = [
-        {
-            title: 'Airbnp', 
-            description: 'Think Airbnb but for bathrooms.', 
-            img: 'airbnp.PNG', 
-            liveLink: 'https://airbnp-website-190a74645719.herokuapp.com/', 
-            codeLink: 'https://github.com/darylbg/airbnp',
-            pills: [
-                {name: 'React', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'MongoDB', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'GraphQL', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'JWT', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'MAPBOX', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-            ]
-        },
-        {
-            title: 'ReadSky', 
-            description: 'A social media platform where users can login or register to share their thoughts and opinions in tweet format. Complete with profile editing, commenting, both user and keyword searching, and trending post displays.',
-            img: 'ReadSky.PNG', 
-            liveLink: 'https://github.com/darylbg/readSky-daryl', 
-            codeLink: 'https://github.com/darylbg/readSky-daryl',
-            pills: [
-                {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'NODEJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'RESTFUL API', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'HANDLEBARS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-            ]
-        },
-        {
-            title: 'Portfolio', 
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra. Maecenas vit vulputate turpis. Fusce Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at eros sed leo dictum viverra.',
-            img: 'ReadSky.PNG', 
-            liveLink: 'https://www.youtube.com/', 
-            codeLink: 'https://github.com/darylbg/db-portfolio',
-            pills: [
-                {name: 'HTML', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'CSS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'Javascript', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'},
-                {name: 'NodeJS', link: 'https://create-react-app.dev/docs/adding-images-fonts-and-files/'}
-            ]
-        }
-    ]
+  {
+    title: "Airbnp",
+    description:
+      "Think Airbnb but for bathrooms. A site where anyone can geolocate themselves and find available, clean, nearby bathrooms to use for a small fee. Featuring a login/logout authentication system, user detail editing, ability to add bathroom listings, ability to locate and book up to payment point nearby bathrooms.",
+    img: "airbnp.PNG",
+    liveLink: "https://airbnp-website-190a74645719.herokuapp.com/",
+    codeLink: "https://github.com/darylbg/airbnp",
+    pills: [
+      {
+        name: "MongoDB",
+        link: "https://www.mongodb.com/",
+      },
 
-    export default projects
+      {
+        name: "express",
+        link: "https://expressjs.com/",
+      },
+      {
+        name: "React",
+        link: "https://react.dev/",
+      },
+
+      {
+        name: "Node js",
+        link: "https://nodejs.org/en",
+      },
+
+      {
+        name: "GraphQL",
+        link: "https://graphql.org/",
+      },
+      {
+        name: "stripe",
+        link: "https://stripe.com/",
+      },
+      {
+        name: "JWT",
+        link: "https://jwt.io/",
+      },
+      {
+        name: "MAPBOX",
+        link: "https://www.mapbox.com/",
+      },
+    ],
+  },
+  {
+    title: "ReadSky",
+    description:
+      "A social media platform where users can login or register to share their thoughts and opinions in tweet format. Complete with profile editing, commenting, both user and keyword searching, and trending post displays.",
+    img: "ReadSky.PNG",
+    liveLink: "https://github.com/darylbg/readSky-daryl",
+    codeLink: "https://github.com/darylbg/readSky-daryl",
+    pills: [
+      {
+        name: "express",
+        link: "https://expressjs.com/",
+      },
+      {
+        name: "mysql",
+        link: "https://www.mysql.com/",
+      },
+      {
+        name: "HANDLEBARS",
+        link: "https://handlebarsjs.com/",
+      },
+      {
+        name: "NODE JS",
+        link: "https://nodejs.org/en",
+      },
+      {
+        name: "RESTFUL API",
+        link: "https://aws.amazon.com/what-is/restful-api/",
+      },
+      {
+        name: "sequelize",
+        link: "https://sequelize.org/",
+      },
+    ],
+  },
+  {
+    title: "Portfolio",
+    description:
+      "This portfolio was designed and built by myself in React. This website was built in React and utilizes Bootstrap as a framework",
+    img: "ReadSky.PNG",
+    liveLink: "https://darylbg.github.io/db-portfolio/",
+    codeLink: "https://github.com/darylbg/db-portfolio",
+    pills: [
+      {
+        name: "react",
+        link: "https://react.dev/",
+      },
+      {
+        name: "bootstrap",
+        link: "https://getbootstrap.com/",
+      },
+      {
+        name: "NodeJS",
+        link: "https://nodejs.org/en",
+      },
+    ],
+  },
+];
+
+export default projects;
